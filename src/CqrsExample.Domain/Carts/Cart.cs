0bd8487b8865389c205a7e6f0293c2925acd1b30
Corey Prater <cprater02@gmail.com>
2022-02-01 12:21:02 -0600

    cleaned up file organization

--- src/CqrsExample.Domain/Carts/Cart.cs
+++ src/CqrsExample.Domain/Carts/Cart.cs
@@ -21,20 +21,3 @@ public class Cart : AggregateRoot
         return Items;
     }
 }
-
-public class CartItem
-{
-    public string Id { get; }
-    public int Quantity { get; private set; }
-
-    public CartItem(string id, int quantity)
-    {
-        Id = id;
-        Quantity = quantity;
-    }
-
-    public void UpdateQuantity(int quantity)
-    {
-        Quantity = quantity;
-    }
-}
---
+++ src/CqrsExample.Domain/Carts/CartItem.cs
@@ -0,0 +1,18 @@
+namespace CqrsExample.Domain.Carts;
+
+public class CartItem
+{
+    public string Id { get; }
+    public int Quantity { get; private set; }
+
+    public CartItem(string id, int quantity)
+    {
+        Id = id;
+        Quantity = quantity;
+    }
+
+    public void UpdateQuantity(int quantity)
+    {
+        Quantity = quantity;
+    }
+}
