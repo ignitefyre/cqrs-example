a86d5d12d27dca80453ea519954afc36c081f4d3
Corey Prater <cprater02@gmail.com>
2022-02-28 22:16:43 -0600

    Added application tests

---
+++ tests/CqrsExample.Application.Tests/AddItemCommandHandlerTests.cs
@@ -0,0 +1,58 @@
+using System.Linq;
+using System.Threading;
+using CqrsExample.Application.Carts;
+using CqrsExample.Application.Carts.Commands;
+using CqrsExample.Application.Carts.Handlers;
+using CqrsExample.Domain.Carts;
+using CqrsExample.Domain.Exceptions;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace CqrsExample.Application.Tests;
+
+public class AddItemCommandHandlerTests : TestBase
+{
+    [Test]
+    public void ShouldAddItemToExistingCart()
+    {
+        //arrange
+        const string cartId = "abc";
+        var cart = new Cart(cartId);
+        
+        var cartRepositoryMock = new Mock<ICartRepository>();
+        cartRepositoryMock.Setup(x => x.GetById(cartId)).Returns(cart);
+        
+        var sut = new AddItemCommandHandler(cartRepositoryMock.Object);
+
+        var request = new AddItemCommand("123", 1, cartId);
+
+        //act
+        var result = sut.Handle(request, CancellationToken.None);
+
+        //assert
+        cart.GetItems().Count().Should().Be(1);
+        cartRepositoryMock.Verify(x => x.Update(cart), Times.Once);
+    }
+    
+    [Test]
+    public void ShouldThrowExceptionWhenNoCartFound()
+    {
+        //arrange
+        const string cartId = "abc";
+        
+        var cartRepositoryMock = new Mock<ICartRepository>();
+        cartRepositoryMock.Setup(x => x.GetById(cartId)).Returns(default(Cart));
+        
+        var sut = new AddItemCommandHandler(cartRepositoryMock.Object);
+
+        var request = new AddItemCommand("123", 1, cartId);
+
+        //act
+        var result = () => sut.Handle(request, CancellationToken.None);
+
+        //assert
+        cartRepositoryMock.Verify(x => x.Update(It.IsAny<Cart>()), Times.Never);
+        result.Should().ThrowAsync<CartNotFoundException>();
+    }
+}
---
+++ tests/CqrsExample.Application.Tests/TestBase.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+
+namespace CqrsExample.Application.Tests;
+
+public class TestBase
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+}
